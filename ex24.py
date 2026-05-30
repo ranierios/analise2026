3662e78ff885319c0aa329d12d9e9e4bb8971390
ranierios <ranieripsy@gmail.com>
2026-05-29 21:33:46 -0300

    29.05.2026

---
+++ ex24.py
@@ -0,0 +1,3 @@
+x = int(input("Digite um valor: "))
+valor = x % 2
+print(valor)
---
+++ ex25.py
@@ -0,0 +1,8 @@
+# ler 5 valores e somar todos eles
+
+m=0
+for i in range(0,5):
+    x=int(input("Digite o valor: "))
+    m = m + x
+    print(f"o valor de m é {m} ")
+
