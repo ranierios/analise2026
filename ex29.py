bb77c62252c156cded9b58da18d75ad36429c30d
ranierios <ranieripsy@gmail.com>
2026-06-01 21:38:58 -0300

    01062026

--- ex29.py
+++ ex29.py
@@ -1,9 +1,9 @@
 
-numeros[ssssssssssss]
+numeros = [3,15,20,33,45,55,70]
 soma = 0
 for numero in numeros:
     soma += numero
-print(f"a soma dos numeros da lista é: ")
+print(f"a soma dos numeros da lista é: {soma} ")
 
 
 
